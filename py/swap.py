f768bf1694703749efb47fb80b8680a36c451baa
justaguy12345 <ryanberk135@gmail.com>
2023-04-18 15:48:45 -0500

    code formatting

--- py/swap.py
+++ py/swap.py
@@ -10,13 +10,11 @@ class SwapTab(customtkinter.CTkTabview):
 def setInitiator(self): #here we are setting the initiator based on the PREVIOUS state of the isInitiator boolean
     if self.isInitiator == True:
         self.isInitiator = False
-#        setSenderChain(self, self.initiatorChainOption.get())
         print("isInitiator = ", self.isInitiator)
         GUI_Arrange_Swap_Based(self)
     else:
         self.isInitiator = True
         print("isInitiator = ", self.isInitiator)
-#        setSenderChain(self, self.initiatorChainOption.get())
         GUI_Arrange_Swap_Based(self)
 
 def determineSwapName():
@@ -42,6 +40,7 @@ def initiateSwap(self):
     def copyENCInit():
             pyperclip.copy(open(self.swap_tab_view.get() + "/ENC_initiation.atomicswap", "r").read()) 
             #make sure active tab functions get swap name from current open tab
+
     if self.initiatorChainOption  == "NotSelected" or self.responderChain == "NotSelected":
         print("at least one chain not selected!")
     elif self.initiatorChainOption == self.responderChain:
@@ -124,6 +123,25 @@ def initiateSwap(self):
                     enc_response = f.read()
                     f.close()
                     pyperclip.copy(enc_response)
+
+            def deployScalarSwapContract():
+                    addr = os.popen("cd Atomicity/" + self.currentswapname + "/ && python3 py/deploy.py").read()
+                    print(addr)
+                    addrfile = open(self.currentswapname + "/responderContractAddress", "w")
+                    addrfile.write(addr)
+                    addrfile.close()
+
+            def goFund():
+                    if os.path.isfile(self.currentswapname + "/responderContractAddress"):
+                        addr = open(self.currentswapname + "/responderContractAddress", "r").read().rstrip()
+                        cmd = "cd Atomicity/" + \
+                                self.currentswapname + " && ./deploy.sh sendAmount " + \
+                                self.swap_tab_view.valueToSpendEntry.get()  + ' '+ addr
+                        print(cmd)
+                        os.popen(cmd).read()          
+                    else:
+                        print("responders contract not found! not deployed yet or recieved")
+
             #make sure active tab functions get swap name from current open tab
             self.currentswapname = determineSwapName()
             if self.swapTabSet == False:
@@ -140,13 +158,11 @@ def initiateSwap(self):
                 decryptElGamal = \
                         "./ElGamal decryptFromPubKey " + self.currentswapname + "/initiation.atomicswap " + \
                         self.currentReceiver + ' ' + self.ElGamalKeyFileName
-                print(decryptElGamal)
                 decryption = os.popen(decryptElGamal).read()
                 f = open(self.currentswapname + "/DEC_initiation.atomicswap", "w")
                 f.write(decryption)
                 f.close()
-                print(decryption)
-                time.sleep(2)
+                time.sleep(1)
                 j = json.loads(decryption)
                 self.counterpartyChainPubkey = j["chainPubkey"]
                 ksG = j["ksG"]
@@ -159,9 +175,8 @@ def initiateSwap(self):
                 cmd = "cd Atomicity && ./new_frame " + self.currentswapname  + \
                         " -M -CA 3 " + "\\\"" + self.counterpartyChainPubkey + "\\\" " \
                         + str(ast.literal_eval(j["xG"])[0])  + " " + str(ast.literal_eval(j["xG"])[1])
-                print(cmd)
                 new_frame = os.popen(cmd)
-                time.sleep(2) #wait for file to be built
+                time.sleep(1) #wait for file to be built
                 #
                 #copy in generic ECC timelock multisig contract for atomic swap
                 os.remove("Atomicity/" + self.currentswapname + "/contracts/" + self.currentswapname + ".sol")
@@ -171,10 +186,8 @@ def initiateSwap(self):
                         "&& cp ../../AtomicMultiSigSecp256k1/contracts/ReentrancyGuard.sol . " + \
                         "&& cp ../../AtomicMultiSigSecp256k1/contracts/EllipticCurve.sol . "
                 cpy = os.popen(contract_copy).read()
-#                print(cpy)
                 time.sleep(1)
                 rename = str(open("Atomicity/" + self.currentswapname + "/contracts/" + self.currentswapname + ".sol", "r").read() )
-#                print(rename.replace('AtomicMultiSigSecp256k1', self.currentswapname))
                 rewrite = open("Atomicity/" + self.currentswapname + "/contracts/" + self.currentswapname + ".sol", "w")
                 rewrite.write(rename.replace('AtomicMultiSigSecp256k1', self.currentswapname))
                 rewrite.close()
@@ -183,13 +196,6 @@ def initiateSwap(self):
                 self.deployAtomicSwapContractLabel = customtkinter.CTkLabel(master=self.swap_tab_view.tab(self.currentswapname), \
                         text="Click to deploy the atomic swap contract: ")
                 self.deployAtomicSwapContractLabel.grid(row=0, column=0, padx=10, pady=10)
-                def deployScalarSwapContract():
-                    addr = os.popen("cd Atomicity/" + self.currentswapname + "/ && python3 py/deploy.py").read()
-                    print(addr)
-                    addrfile = open(self.currentswapname + "/responderContractAddress", "w")
-                    addrfile.write(addr)
-                    addrfile.close()
-
                 self.deployAtomicSwapButton = customtkinter.CTkButton(master=self.swap_tab_view.tab(self.currentswapname), \
                         text="Deploy", command=deployScalarSwapContract)
                 self.deployAtomicSwapButton.grid(row=1, column=0, padx=10, pady=10)
@@ -203,27 +209,9 @@ def initiateSwap(self):
                 self.counterpartyChainPubkeyLabel = customtkinter.CTkLabel(master=self.swap_tab_view.tab(self.currentswapname), \
                         text="Counterparty ChainPubkey: " + self.counterpartyChainPubkey)
                 self.counterpartyChainPubkeyLabel.grid(row=2, column=0, padx=10, pady=10)
-                '''
-                self.swap_tab_view.labelresponse = customtkinter.CTkLabel(master=self.swap_tab_view.tab(self.currentswapname), \
-                    text="Click to copy generated response commitments: ")
-                self.swap_tab_view.labelresponse.grid(row=3, column=0, padx=10, pady=10)
-                self.swap_tab_view.copyResponseButton = \
-                        customtkinter.CTkButton(master=self.swap_tab_view.tab(self.currentswapname), \
-                        text="Copy", command=copyResponse)
-                self.swap_tab_view.copyResponseButton.grid(row=4, column=0, padx=10, pady=10)
-                '''
+
                 #TODO: WARN USER about sending funds!!
                 #TODO: make value entry based on chain aka wei or sats
-                def goFund():
-                    if os.path.isfile(self.currentswapname + "/responderContractAddress"):
-                        addr = open(self.currentswapname + "/responderContractAddress", "r").read().rstrip()
-                        cmd = "cd Atomicity/" + \
-                                self.currentswapname + " && ./deploy.sh sendAmount " + \
-                                self.swap_tab_view.valueToSpendEntry.get()  + ' '+ addr
-                        print(cmd)
-                        os.popen(cmd).read()          
-                    else:
-                        print("responders contract not found! not deployed yet or recieved")
                 self.swap_tab_view.valueLabel = customtkinter.CTkLabel(master=self.swap_tab_view.tab(self.currentswapname), \
                     text="Amount to spend in wei:")
                 self.swap_tab_view.valueLabel.grid(row=5, column=0, padx=10, pady=10)
